2d76b81f9635665559fc2c8544b4e972216a0881
shiretoko219 <shiretoko@163.com>
2022-08-11 14:52:08 +0800

    A Java assignment for java study and test for git.  一个java学习作业，兼以测试git。

---
+++ SuperPlus.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+//一个输入某数可以一直加到100的简单程序
+public class SuperPlus {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个小于100的整数：");
+        int num = sc.nextInt();
+        int a=num;  //第一个加数a
+        int b = 0;  //b充当结果暂存
+        do {
+            b=b+(a++);  //第一次相加后a自加，充当下一个加数
+        }
+         while (a <= 100);{
+            System.out.println("结果是："+b);
+        }
+        sc.close();
+    }
+}
