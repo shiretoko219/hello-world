19637d92213f869748e97bbdc6df5755cfbf993f
shiretoko219 <shiretoko@163.com>
2022-09-15 23:31:50 +0800

    Correct errors of Newton's Method.

--- NRMethod.java
+++ NRMethod.java
@@ -6,7 +6,7 @@ public class NRMethod {
         System.out.println("本程序将用牛顿迭代法计算函数的根");
         System.out.println("请输入预估的根：");
         double x0 = sc.nextDouble();
-        System.out.println("请输入允许误差（小数）");
+        System.out.println("请输入允许误差（小数）：");
         double e = sc.nextDouble();
         double x1 = x0 - TheFunc(x0) / DevFunc(x0);
         System.out.println("结果为：");
@@ -14,7 +14,7 @@ public class NRMethod {
         sc.close();
 }
     public static double NRMethod(double x0,double e,double x1) {
-        Double ein0,ein1;  //ein0储存计算误差和设置e比较，ein1负责储存计算结果
+        double ein0;//ein0储存计算误差和设置e比较，ein1负责储存计算结果
         double x = x0;
         double newX = x1;
         do {               //牛顿迭代法的计算
@@ -23,12 +23,11 @@ public class NRMethod {
             newX = newX-((newX-x)/(TheFunc(newX)-TheFunc(x))*TheFunc(newX));
             double miu = save-newX;
             ein0 = Math.abs(miu);
-            ein1 = newX;
+
         }
-        while (ein0 < e);  //小于误差时，数出结果
-        {
-            return ein1;
-        }
+        while (e<=ein0);  //小于误差时，数出结果
+        return newX;
+
     }
     private static double TheFunc(double x){  //原函数
         double y = x*Math.log10(x)-1;
